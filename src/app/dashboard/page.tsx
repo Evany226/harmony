7591115ebb3288d3a6411812ee777628d5de2fe5
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-06-16 13:21:19 -0400

    basic setup. gonna build navbar first

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import SideNav from "@/app/ui/side-nav";
+
+export default function Dashboard() {
+    return (
+        <main className="w-full h-[100vh] bg-gray-100">
+            <SideNav />
+        </main>
+    )
+}
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Noto_Sans} from "next/font/google"
+
+export const noto = Noto_Sans({weight: ["400","500","600","700"] , subsets:['latin']})
+
