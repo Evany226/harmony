319b0c3ca62924659dee0ad20cae7166c8c9e694
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-06-19 15:41:08 -0400

    prisma setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,42 @@
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
 
-const app = express();
+// const app = express();
 
-app.use(cors);
-app.use(express.json());
+// app.use(cors);
+// app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World",
+        },
+      },
+    },
+  });
+  console.log("Created new user: ", newUser);
+
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  });
+  console.log("All users: ");
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect());
