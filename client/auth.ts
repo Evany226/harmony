327cf4f609060cd2f9fddd1c5984657b1206c652
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-06-23 21:15:53 -0400

    nextauth issues
    got the google provider working but app not redirecting on login, will fix later

---
+++ client/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+});
+
+// import NextAuth from "next-auth";
+// import Google from "next-auth/providers/google";
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [Google],
+// });
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,3 +1,22 @@
-export default function Login() {
-  return <div>login</div>;
+import { signIn, auth } from "../../../auth";
+
+export default async function Login() {
+  const session = await auth();
+
+  return (
+    <div>
+      <h1>This is the login page</h1>
+      <p>Signed in as {session?.user?.email}</p>;
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google", { callbackUrl: "/dashboard" });
+        }}
+      >
+        <button type="submit" className="bg-red-100">
+          Signin with Google
+        </button>
+      </form>
+    </div>
+  );
 }
